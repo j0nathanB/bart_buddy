a702ddd2f4a639e4aa4e43e85cce4aff224b82d1
Greg Coffeng <gcoffeng@gmail.com>
2017-06-02 18:59:24 -0700

    add drop down menus to trainRoutes and station component

--- client/src/components/station.js
+++ client/src/components/station.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import stationLat_and_Long from './station_coordinates'
 
-class Station extends React.Component {
+const styles = {
+  customWidth: {
+    width: 150,
+  },
+};
+
+const items = [];
+
+for (let j = 0; j < stationLat_and_Long.length; j++) {
+  items.push(<MenuItem value={j} key={j} primaryText={`${stationLat_and_Long[j].name}` } />);
+}
+items.push(<MenuItem value={stationLat_and_Long.length} key={stationLat_and_Long.length} primaryText={"Use Location" } />);
+
+class station extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = { 
-    };
+    this.state = {value: 0};
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  render () {
+  handleChange(event, index, value) {
+    this.setState({value: value});
+  }
+
+  render() {
     return (
-      <div>
-        <h1>Station: Powell</h1>
-      </div>
+      <DropDownMenu maxHeight={250} value={this.state.value} onChange={this.handleChange} style={styles.customWidth}>
+        {items}
+      </DropDownMenu>
     );
   }
 }
 
-export default Station;
+export default station
--- client/src/components/trainRoutes.js
+++ client/src/components/trainRoutes.js
@@ -1,19 +1,56 @@
 import React from 'react';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import stationLat_and_Long from './station_coordinates'
+
+const styles = {
+  customWidth: {
+    width: 200,
+  },
+};
+
+var hardCodedTrainRoutes = [ 
+  'select-route',
+  'PITT-SFIA',
+  'DALY-DUBL',
+  'DALY-WARM',
+  'DUBL-DALY',
+  'WARM-DALY',
+  'WARM-RICH',
+  'MLBR-RICH',
+  'RICH-WARM',
+  'RICH-MLBR',
+  'SFIA-PITT',
+  'COLS-OAKL',
+  'OAKL-COLS' ];
+
+// PITT-SFIA
+
+const items = [];
+
+for (let j = 0; j < hardCodedTrainRoutes.length; j++) {
+  items.push(<MenuItem value={j} key={j} primaryText={`${hardCodedTrainRoutes[j]}` } />);
+  console.log()
+}
 
 class TrainRoutes extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-    };
+    this.state = {value: 0};
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  render () {
+  handleChange(event, index, value) {
+    this.setState({value: value});
+  }
+
+  render() {
     return (
-      <div>
-        <h1>TrainRoutes: Richmond</h1>
-      </div>
+      <DropDownMenu maxHeight={300} value={this.state.value} onChange={this.handleChange} style={styles.customWidth}>
+        {items}
+      </DropDownMenu>
     );
   }
 }
 
-export default TrainRoutes;
+export default TrainRoutes
